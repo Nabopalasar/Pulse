c8e8f8d3f257b99032b9c9ff5d65eaed7f0568ee
Vyacheslav <vyacheslavburyy@gmail.com>
2022-06-27 14:19:02 +0300

    Adder carousel section

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 900,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-left-solid.png"></img></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right-solid.png"></img></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                      dots: true,
+                      arrows: false
+                }
+              }
+        ]
+      });
+  });
+  
