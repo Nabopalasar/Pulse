004b1d82e7920d4a54ef211f7e800e18f7fe9ee4
Vyacheslav <vyacheslavburyy@gmail.com>
2022-06-27 21:21:46 +0300

    Added tabs changer and tabsList toggler JS

--- src/js/main.js
+++ src/js/main.js
@@ -33,4 +33,63 @@ const slider = tns({
     slider.goTo('next');
   });
 
-  
+  //TABS
+const tabsParent = document.querySelector('.catalog__tabs'),
+      tabs = document.querySelectorAll('.catalog__tab'),
+      tabsText = document.querySelectorAll('.catalog-text')
+      tabContent = document.querySelectorAll('.catalog__content'),
+      catalogContent = document.querySelectorAll('.catalog-item__content'),
+      catalogList = document.querySelectorAll('.catalog-item__list')
+      catalogDetail = document.querySelectorAll('.catalog-item__link'),
+      catalogBack = document.querySelectorAll('.catalog-item__back');
+
+function hideTabContent() {
+  tabContent.forEach(item => {
+    item.style.display = "none";
+  });
+  tabs.forEach(item => {
+    item.classList.remove('catalog__tab_active');
+  });
+}
+
+function showTabsContent(i = 0) {
+  tabContent[i].style.display = 'flex';
+  tabs[i].classList.add('catalog__tab_active');
+}
+
+function toggleTabList(i) {
+  catalogContent[i].classList.toggle('catalog-item__content_active');
+  catalogList[i].classList.toggle('catalog-item__list_active');
+}
+
+catalogDetail.forEach((item, i) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleTabList(i);
+
+  });
+});
+
+catalogBack.forEach((item, i) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleTabList(i);
+  });
+});
+
+hideTabContent();
+showTabsContent();
+
+tabsText.forEach(item => {
+  item.addEventListener('click', (e) => {
+    const target = e.target;
+    if(target) {
+      tabsText.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabsContent(i);
+        }
+      });
+    }
+  });
+});
